e1a6c022f14c2f6603b0e84733268a1c14f95c33
nagasaki <hayabusa_nagasaki@yahoo.co.jp>
2020-11-24 21:22:07 +0900

    material

---
+++ ddl/createTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE
+house
+(id int AUTO_INCREMENT PRIMARY KEY,
+name varchar(255),
+material_id_csv varchar(255))DEFAULT CHARSET=utf8;
+
+INSERT INTO
+house
+VALUES ('', 'test','1,2,3');
--- index.php
+++ index.php
@@ -1,1 +1,5 @@
 <?php
+require './vendor/autoload.php';
+$smarty = new Smarty();
+$smarty->setTemplateDir('templates/templates')->setCacheDir('templates/cache')->setCompileDir('templates/templates_c')->setCacheDir('templates/cache')->setConfigDir('templates/configs');
+$smarty->display('templates/style.tpl');
