632f8505ed8a7059b7e86215ba30673ed144e45a
nagasaki <hayabusa_nagasaki@yahoo.co.jp>
2020-11-30 17:32:25 +0900

    material

--- dbc.php
+++ dbc.php
@@ -1,5 +1,6 @@
 <?php
-$dsn = 'mysql:dbname=	q9jovleh3442yuxk;host=localhost;dbname=q9jovleh3442yuxk;charset=utf8';
+$dsn = 'mysql:dbname=q9jovleh3442yuxk;
+host=i943okdfa47xqzpy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;charset=utf8';
 $user = 'vtv47qw6y2tti1hf';
 $pass = 'i6vakirb7fic8543';
 
@@ -8,7 +9,7 @@ try{
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     ]);
     echo'接続成功';
-    $dbh =null
+    $dbh =null;
 } chtch(PDDException $e){
     echo '接続失敗'. $e->getMessage();
     exit();
