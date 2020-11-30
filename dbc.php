acd4ac98114076e701538ee21ffbf21d58cd347e
nagasaki <hayabusa_nagasaki@yahoo.co.jp>
2020-11-30 23:58:27 +0900

    material

--- dbc.php
+++ dbc.php
@@ -1,4 +1,5 @@
 <?php
+echo test == 1;
 $dsn = 'mysql:dbname=q9jovleh3442yuxk;
 host=i943okdfa47xqzpy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;charset=utf8';
 $user = 'vtv47qw6y2tti1hf';
@@ -6,17 +7,17 @@ $pass = 'i6vakirb7fic8543';
 
 
 try{
-    $bdh = new PDO ($dsn,$user,$pass,
+    $bdh = new PDO($dsn, $user, $pass,
     [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    ]);
+    ] ) ;
     echo'接続成功';
     $sql = 'SELECT * FROM house';
-$prepare = $dbh->prepare($sql);
-//$prepare->bindValue(':id', 3, PDO::PARAM_INT);
-$prepare->execute();
-$result = $prepare->fetchAll(PDO::FETCH_ASSOC);
+$stmt = $dbh->prepare($sql);
+//$stmt->bindValue(':id', 3, PDO::PARAM_INT);
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-var_dump($result);
+var_dump ($result);
 } catch(PDDException $e){
     echo '接続失敗'. $e->getMessage();
     exit();
