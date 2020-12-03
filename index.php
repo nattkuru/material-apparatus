9a932d84ad130b65495c62ff9c8a6ba2656f3ec5
nagasaki <hayabusa_nagasaki@yahoo.co.jp>
2020-12-03 20:57:57 +0900

    material

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ $smarty = new Smarty();
 $smarty->setTemplateDir('templates/templates')->setCacheDir('templates/cache')->setCompileDir('templates/templates_c')->setCacheDir('templates/cache')->setConfigDir('templates/configs');
 
 $housename = getHousename();
-$smarty->assign('housename',print_r($housename,true));
+$smarty->assign('housename',$housename);
 $smarty->display('templates/style.tpl');
